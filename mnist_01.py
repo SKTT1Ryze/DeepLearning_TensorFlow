8f3e482ce589bba3dd9f7abeafb1c3e721e997c6
SKTT1Ryze <1276675421@qq.com>
2019-12-02 09:58:35 +0800

    model training for mnist dataset

---
+++ mnist_01.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import os, shutil
+import tensorflow as tf
+import tensorflow_core
+from tensorflow_core import keras
+from tensorflow_core.keras import layers
+from tensorflow_core.keras import Sequential
+from tensorflow_core.keras import optimizers, losses
+# from tensorflow.examples.tutorials.mnist import input_data
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = tf.convert_to_tensor(x_train, dtype=tf.float32) / 255.
+y_train = tf.convert_to_tensor(y_train, dtype=tf.int32)
+y_train = tf.one_hot(y_train, depth=10)
+print(x_train.shape, y_train.shape)
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(200)
+
+# networks = Sequential([
+#     layers.Conv2D(6, kernel_size=3, strides=1),  # 第一个卷积层，6个3*3卷积核
+#     layers.MaxPooling2D(pool_size=2, strides=2),  # 高宽各减半的池化层
+#     layers.ReLU(),  # 激活函数
+#     layers.Conv2D(16, kernel_size=3, strides=1),  # 第二个卷积层,16g个3*3卷积核
+#     layers.MaxPooling2D(pool_size=2, strides=2),  # 高宽各减半的池化层
+#     layers.ReLU(),  # 激活函数
+#     layers.Flatten(),  # 打平层
+#
+#     layers.Dense(120, activation='relu'),  # 全连接层，120个节点
+#     layers.Dense(84, activation='relu'),  # 全连接层，84个节点
+#     layers.Dense(10)  # 全连接层，10个节点
+# ])
+
+# networks.build(input_shape=(4, 28, 28, 1))  # build
+# networks.summary()
+# criteon = losses.CategoricalCrossentropy(from_logits=True)
+optimizer = optimizers.SGD(learning_rate=0.001)
+
+networks = keras.Sequential([layers.Dense(784, activation='relu'),
+                             layers.Dense(256, activation='relu'),
+                             layers.Dense(10)])
+
+
+def train_epoch(epoch):
+    for step, (x, y) in enumerate(train_dataset):
+        with tf.GradientTape() as tape:
+            x = tf.reshape(x, (-1, 28 * 28))
+            out = networks(x)
+            loss = tf.reduce_sum(tf.square(out - y)) / x.shape[0]
+        grad = tape.gradient(loss, networks.trainable_variables)
+        optimizer.apply_gradients(zip(grad, networks.trainable_variables))
+        if step % 100 == 0:
+            print("数据集迭代第%s次，训练step为%s，loss函数为：%s" % (epoch, step, loss.numpy()))
+
+
+def train():
+    for epoch in range(30):
+        train_epoch(epoch)
+
+
+if __name__ == '__main__':
+    train()
---
+++ mnist_02.py
@@ -0,0 +1,93 @@
+import numpy as np
+# import pandas as pd
+import os, shutil
+import tensorflow as tf
+# import tensorflow_core
+# from tensorflow_core import keras
+# from tensorflow_core.keras import layers
+# from tensorflow_core.keras import Sequential
+# from tensorflow_core.keras import optimizers, losses
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+batch_size = 128
+nb_classes = 10
+epochs = 12
+
+img_rows, img_cols = 28, 28
+nb_filters = 32
+pool_size = (2, 2)
+kernel_size = (3, 3)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+y_train = np_utils.to_categorical(y_train, nb_classes)
+y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+model.add(Convolution2D(nb_filters, (kernel_size[0], kernel_size[1]),
+                        padding='same',
+                        input_shape=input_shape))  # 卷积层1
+model.add(Activation('relu'))  # 激活层
+model.add(Convolution2D(nb_filters, (kernel_size[0], kernel_size[1])))  # 卷积层2
+model.add(Activation('relu'))  # 激活层
+model.add(MaxPooling2D(pool_size=pool_size))  # 池化层
+model.add(Dropout(0.25))  # 神经元随机失活
+model.add(Flatten())  # 拉成一维数据
+model.add(Dense(128))  # 全连接层1
+model.add(Activation('relu'))  # 激活层
+model.add(Dropout(0.5))  # 随机失活
+model.add(Dense(nb_classes))  # 全连接层2
+model.add(Activation('softmax'))  # Softmax评分
+
+# model = Sequential([
+#     Convolution2D(6, kernel_size=3, strides=1, input_shape=input_shape),  # 第一个卷积层，6个3*3卷积核
+#     Activation('relu'),  # 激活函数
+#     MaxPooling2D(pool_size=2, strides=2),  # 高宽各减半的池化层
+#     Activation('relu'),  # 激活函数
+#     Convolution2D(16, kernel_size=3, strides=1),  # 第二个卷积层,16个3*3卷积核
+#     Activation('relu'),  # 激活函数
+#     MaxPooling2D(pool_size=2, strides=2),  # 高宽各减半的池化层
+#     Activation('relu'),  # 激活函数
+#     Flatten(),  # 打平层
+#
+#     Dense(120, activation='relu'),  # 全连接层，120个节点
+#     Activation('relu'),  # 激活函数
+#     Dense(84, activation='relu'),  # 全连接层，84个节点
+#     Activation('relu'),  # 激活函数
+#     Dense(10),  # 全连接层，10个节点
+#     Activation('softmax')  # 激活函数
+# ])
+
+# model.compile(loss='categorical_crossentropy',
+#               optimizer='adadelta',
+#               metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.build(input_shape=(4, img_rows, img_cols, 1))
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
+          verbose=1, validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
